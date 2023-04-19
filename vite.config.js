f872258a8735adae85ece3c7a463a46a6fe91914
hottnerd <111224077+hottnerd@users.noreply.github.com>
2023-04-19 20:43:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
-      "^/api":{
-        target: "https://my-note-api-u30q.onrender.com",
-        changeOrigin: true,
-        secure: true,      
-        ws: true,
-      }
+    
     
     }
-  }
 })
