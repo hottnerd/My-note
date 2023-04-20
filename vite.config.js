fb82c58fa098b22311a858e39ce3a7e4772e8068
hottnerd <111224077+hottnerd@users.noreply.github.com>
2023-04-20 14:17:09 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const API_BASE_URL = import.meta.env.API_BASE_URL;
+
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy:{
       "^/api":{
-        target: API_BASE_URL,
+        target: "https://my-note-api-u30q.onrender.com",
         changeOrigin: true,
         secure: true,      
         ws: true,
