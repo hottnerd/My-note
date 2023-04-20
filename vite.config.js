6b8b5bba0c2a664fedc39f17751ac6db4cb28d93
hottnerd <111224077+hottnerd@users.noreply.github.com>
2023-04-20 15:11:29 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,11 @@ export default defineConfig({
     proxy:{
       "^/api":{
         target: "https://my-note-api-u30q.onrender.com",
-        changeOrigin: true,
-        secure: true,      
+        secure: true,  
+        ws: true,
       }
     
     }
   }
 })
+
